24b9339f44b2d0f143b7fa7fa21cd10d44cecf45
bobnow <emavinil@gmail.com>
2024-04-02 04:09:18 +0300

    Expand the caching system

--- src/liaa/io.py
+++ src/liaa/io.py
@@ -32,21 +32,39 @@ if CONF['font']:
 else:
     font = pygame.font.SysFont(pygame.font.get_default_font(), 10*SIZE)
 
-#
-# GET ASSETS
-#
+# Cache operations
 
+def cacheObjects(name):
+    newCache = {}
+    for i in name:
+        newCache[i] = getAsset(i)
+    global cache
+    cache = newCache.copy()
+
+# Asset stuff
+
+def getAsset(name):
+    global cache
+    if name in cache:
+        return cache[name]
+    else:
+        match name.split('.')[1][:4]:
+            case 'mp3':
+                return getSound(name.split('.')[0])
+            case 'png':
+                return getPicture(name.split('.')[0])
+            case 'png/':
+                return getPicture(name)
 
 def getSound(name):
     global cache
+    if f'{name}.mp3' in cache:
+        return cache[f'{name}.mp3']
     path = merge(PATH, 'resources', 'sounds', f'{name}.mp3')
     if os.path.exists(path):
-        if path in cache:
-            return cache[path]
-        else:
-            sound = pygame.mixer.Sound(path)
-            cache[path] = sound
-            return sound
+        sound = pygame.mixer.Sound(path)
+        cache[f'{name}.mp3'] = sound
+        return sound
     else:
         return None
 
@@ -54,14 +72,24 @@ def getRes():
     return (RES[0] * SIZE, RES [1] * SIZE)
 
 def getPicture(name):
-    path = merge(PATH, 'resources', 'textures', f'{name}.png')
-    if os.path.exists(path):
-        return path
+    path = ''
+    if '.' in name:
+        # Sprite sheet logic goes here
+        # TODO: Add sprite sheet logic
+        pass
     else:
-        return merge(PATH, 'resources', 'textures', 'missing.png')
-
-def getPygameSurface(path):
+        path = merge(PATH, 'resources', 'textures', f'{name}.png')
+    global cache
+    if path in cache:
+        return cache[path]
+    elif not os.path.exists(path):
+        path = merge(PATH, 'resources', 'textures', 'missing.png')
     i = pygame.image.load(path)
+    cache[path] = i
+    return i
+
+def _getPygameSurface(path):
+    i = getPicture(path)
     i = pygame.transform.scale(i, (i.get_rect().width*SIZE, i.get_rect().height*SIZE))
     return i
 
@@ -71,10 +99,10 @@ def getTexture(name):
         # Animation not found
         if os.path.exists(merge(PATH, 'resources', 'textures', f'{name}.png')):
             # It's just a plain texture
-            return ((getPygameSurface(getPicture(name)), (0, 0)),)
+            return ((_getPygameSurface(name), (0, 0)),)
         else:
             # Dev is stupid and called for a nonexistant texture
-            return ((getPygameSurface(getPicture('missing')), (0, 0)), (getPygameSurface(getPicture('error')), (0, 0)))
+            return ((_getPygameSurface('missing'), (0, 0)), (_getPygameSurface('error'), (0, 0)))
     
 
     try:
@@ -86,21 +114,23 @@ def getTexture(name):
 
     except:
         # Error animation
-        return ((getPygameSurface(getPicture('missing')), (0, 0)), (getPygameSurface(getPicture('error')), (0, 0)))
+        return ((_getPygameSurface('missing'), (0, 0)), (_getPygameSurface('error'), (0, 0)))
 
 #
-# SAVE AND LOAD GAME
+# Save and load
 #
 
-def saveScene(name, path):
+def saveScene(name, path, saveCache = True):
     path = merge(PATH, 'scenes', f'{path}.bin')
     # Very bad way to do this, but it works.
     try:
         open(path, 'x')
     except:
         pass
-    pickle.dump(scene, open(path, 'wb'))
+    pickle.dump([saveCache*list(cache.keys()), scene], open(path, 'wb'))
 
 def loadScene(path):
     path = merge(PATH, 'scenes', f'{path}.bin')
-    return pickle.load(open(path, 'rb'))
+    c, scene = pickle.load(open(path, 'rb'))
+    cacheObjects(c)
+    return scene
